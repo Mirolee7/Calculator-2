c515c741f5b8b892d5f7d8261b35f80d7d314fcf
Mirolee7 <muhammadalabi493@gmail.com>
2025-08-22 17:39:42 +0100

    i added main file
    i added main file

---
+++ main.py
@@ -0,0 +1,16 @@
+from calculator.addition.add import add
+from calculator.subtraction.subtract import subtract
+from calculator.multiplication.multiply import multiply
+from calculator.division.divide import divide
+from calculator.checkers.is_even_is_odd import is_even_odd
+
+
+print(add(1,2))
+print(subtract(2,1))
+print(multiply(2,4))
+print(divide(4,2))
+print(is_even_odd(5))
+print(is_even_odd(4))
+
+
+
